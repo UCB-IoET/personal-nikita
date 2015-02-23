7b36ca595ae60e16fea3a1e01cbe7a8ea4f5be15
Nikita Kitaev <nikitakit@gmail.com>
2015-02-22 17:52:00 -0800

    Copy of files for the toaster onboard processor

--- toaster/temp.lua
+++ toaster/temp.lua
@@ -1,102 +1,49 @@
-REG = require("reg")
-require("storm")
-require("cord")
-TMP006_CONFIG = 0x02
-TMP006_T_AMBIENT = 0x01
-TMP006_MFG_ID = 0xfe
-TMP006_DEVICE_ID = 0xff
+require("storm") -- libraries for interfacing with the board and kernel
+require("cord") -- scheduler / fiber library
+ADC = require ("adc") -- adc library
 
---Config register values.
-TMP006_CFG_RESET    = 0x80
-TMP006_CFG_MODEON   = 0x70
-CFG_1SAMPLE         = 0x00
-CFG_2SAMPLE         = 0x02
-CFG_4SAMPLE         = 0x04
-CFG_8SAMPLE         = 0x06
-CFG_16SAMPLE        = 0x08
-TMP006_CFG_DRDYEN   = 0x01
-TMP006_CFG_DRDY     = 0x80
-
+----------------------------------------------
+-- TEMP class
+--   basic TEMP functions associated with a shield pin
+--   assume cord.enter_loop() is active, as per stormsh
+----------------------------------------------
 local TEMP = {}
 
 function TEMP:new()
-   local obj = {port=storm.i2c.INT, addr = 0x80, 
-                reg=REG:new(storm.i2c.INT, 0x80)}
-   setmetatable(obj, self)
+   local s = ADC:new()
+   local active = false
+   cord.new(function() 
+       rv = s:init() 
+
+	  if (rv ~= 0) then
+		 print "Error initializing"
+	  else
+		 print "Done"
+		 active = true
+	  end
+   end)
+   local obj = {}		-- initialize the new object
+
+   obj.temp = 0
+   cord.new(function()
+      while (active == false) do
+         cord.await(storm.os.invokeLater, 250*storm.os.MILLISECOND)
+      end
+      while(active) do -- FIXME make inactive eventually
+         obj.temp = s:get()
+         cord.await(storm.os.invokeLater, 250*storm.os.MILLISECOND)
+      end
+   end)
+
+   setmetatable(obj, self)	-- associate class methods
    self.__index = self
    return obj
 end
 
-
-function TEMP:init()
-    --configure the conversion rate (0x02 0x00 = 2 sample/second, 0x04 0x00 = 1 s/s)
-    self.reg:w16(TMP006_CONFIG, 0x00 + 0x70 + 0x01, 0x00);
-end
-
 function TEMP:getTemp()
-    --Read ambient temperature
-    local addr = storm.array.create(1, storm.array.UINT8)
-    addr:set(1, TMP006_T_AMBIENT)
-    local rv = cord.await(storm.i2c.write,  self.port + self.addr,  storm.i2c.START, addr)
-    if (rv ~= storm.i2c.OK) then
-        print ("ERROR ON I2C: ",rv)
-    end
-    local dat = storm.array.create(2, storm.array.UINT8)
-    rv = cord.await(storm.i2c.read,  self.port + self.addr,  storm.i2c.RSTART + storm.i2c.STOP, dat)
-    if (rv ~= storm.i2c.OK) then
-        print ("ERROR ON I2C: ",rv)
-    end
-    --Converting temperature into Celsius (each LSB = 1/32 Deg. Celsius)
-    local temperature = dat:get_as(storm.array.INT16_BE, 0)/(32*4) --Divide by 4, datasheet spec
-    return temperature
-end
-
-function TEMP:getStatus()
-    -- Read configuration register
-    local addr = storm.array.create(1, storm.array.UINT8)
-    addr:set(1, TMP006_CONFIG)
-    local rv = cord.await(storm.i2c.write,  self.port + self.addr,  storm.i2c.START, addr)
-    if (rv ~= storm.i2c.OK) then
-        print ("ERROR ON I2C: ",rv)
-    end
-    local dat = storm.array.create(2, storm.array.UINT8)
-    rv = cord.await(storm.i2c.read,  self.port + self.addr,  storm.i2c.RSTART + storm.i2c.STOP, dat)
-    if (rv ~= storm.i2c.OK) then
-        print ("ERROR ON I2C: ",rv)
-    end
-    --reading 8th bit of configuration, which indicates if conversion is ready
-    local config = bit.band((dat:get_as(storm.array.INT16_BE, 0)/128), 0x01)
-    return config
-end
-
-function TEMP:get_MFG_DEV_ID()
-    --Read Mfg ID, should be 21577
-    addr:set(1, TMP006_MFG_ID)
-    local rv = cord.await(storm.i2c.write,  self.port + self.addr,  storm.i2c.START, addr)
-    if (rv ~= storm.i2c.OK) then
-        print ("ERROR ON I2C: ",rv)
-    end
-    local dat = storm.array.create(2, storm.array.UINT8)
-    rv = cord.await(storm.i2c.read,  self.port + self.addr,  storm.i2c.RSTART + storm.i2c.STOP, dat)
-    if (rv ~= storm.i2c.OK) then
-        print ("ERROR ON I2C: ",rv)
-    end
-    local mfg_id = dat:get_as(storm.array.INT16_BE, 0)
-
-    --Read Device ID, should be 103
-    addr:set(1, TMP006_DEVICE_ID)
-    local rv = cord.await(storm.i2c.write,  self.port + self.addr,  storm.i2c.START, addr)
-    if (rv ~= storm.i2c.OK) then
-        print ("ERROR ON I2C: ",rv)
-    end
-    local dat = storm.array.create(2, storm.array.UINT8)
-    rv = cord.await(storm.i2c.read,  self.port + self.addr,  storm.i2c.RSTART + storm.i2c.STOP, dat)
-    if (rv ~= storm.i2c.OK) then
-        print ("ERROR ON I2C: ",rv)
-    end
-    local device_id = dat:get_as(storm.array.INT16_BE, 0)
-    
-    return mfg_id, device_id
+    return self.temp
+   -- temp_constant = 1 --XXX: Figure this out by testing...
+   -- return storm.io.get(storm.io[self.pin]) * temp_constant
 end
 
 return TEMP
